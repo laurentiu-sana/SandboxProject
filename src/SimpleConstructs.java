583291c2ff11fb02c365f9c27967a3ce03501aa0
Laurentiu Dascalu <laurentiu.dascalu@epfl.ch>
2012-10-26 00:20:51 -0700

    dummy commit

---
+++ src/SimpleConstructs.java
@@ -0,0 +1,17 @@
+
+
+public class SimpleConstructs {
+    public static void main(String[] args) {
+        int x = 0, y = 1, n = 10;
+
+        for (int i = 0; i < n; i++) {
+            x = x + y;
+        }
+
+        int u = 1, v = 2;
+        for (int j = 0; j < n; j++) {
+            u = u + v;
+        }
+
+    }
+}
